d36c05739ae6e72131eb82335d32c8bda3e69b57
Shameen Shetty <shettyshameen1@gmail.com>
2022-08-02 16:10:25 -0500

    minor changes

---
+++ lib/model/lottery_ticket.dart
@@ -0,0 +1,10 @@
+class LotteryTicket {
+  final String ticketNumber;
+  final String ticketName;
+  final int ticketPrice;
+
+  const LotteryTicket(
+      {required this.ticketNumber,
+      required this.ticketName,
+      required this.ticketPrice});
+}
